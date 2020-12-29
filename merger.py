6768605046bde68a8acf2f314929c5c85d46d82d
Ilia <fortyhell@gmail.com>
2020-12-29 11:40:46 +0300

    Split logic to functions

--- merger.py
+++ merger.py
@@ -3,28 +3,52 @@ import os
 import yaml
 
 def get_file_name(path, folder, file, extension):
-	return path + '/' + folder + '/' + file + '.' + extension
+	# when folder == '' result path looks like 'path//file.extension'
+	if folder != '':
+		folder += '/'
+	return path + '/' + folder + file + '.' + extension
+
+def load_config(config_path):
+	try:
+		with open(config_path, 'r') as stream:
+			config = yaml.safe_load(stream)
+		return config
+	except Exception as e:
+		quit(str(e) + '\nPlease create config.yml')
+
+def ask_overwrite(file_name):
+	if os.path.exists(file_name):
+		answer = input('File "%s" already exist. Overwrite? [y, n] ' % file_name)
+		if answer == 'n':
+			return False
+		elif answer == 'y':
+			# clear file content
+			open(file_name, 'w').close()
+			return True
+
+def merge_files(result_name, files):
+	result_file = open(result_name, 'a')
+	for file in files:
+		file_name = get_file_name(current_path, input_folder, file, config['extension'])
+		try:
+			with open(file_name, 'r') as f:
+				result_file.write(f.read())
+		except Exception as e:
+			print(e)
 
 if __name__ == '__main__':
 	script_path = sys.path[0]
 
-	# by default config located where script located
-	if len(sys.argv) > 1:
-	    config_path = sys.argv[1]
-	else:
-	    config_path = script_path + '/config.yml'
-
 	# from where script run
 	current_path = os.getcwd()
 
-	# load config
-	try:
-		with open(config_path, 'r') as stream:
-			config = yaml.safe_load(stream)
-	except Exception as e:
-		quit(str(e) + '\nPlease create config.yml')
+	# by default config located where script located
+	if len(sys.argv) > 1:
+	    config_path = current_path + '/' + sys.argv[1]
+	else:
+	    config_path = script_path + '/config.yml'
 
-	print(config)
+	config = load_config(config_path)
 
 	if 'folder' in config:
 		input_folder = config['folder']
@@ -33,21 +57,7 @@ if __name__ == '__main__':
 
 	result_name = get_file_name(current_path, input_folder, config['output'], config['extension'])
 
-	# check file existing
-	if os.path.exists(result_name):
-		answer = input('File already exist. Overwrite? [y, n]')
-		if answer == 'n':
-			quit('Exiting')
-		elif answer == 'y':
-			# clear file content
-			open(result_name, 'w').close()
+	if not ask_overwrite(result_name):
+		quit('Exiting')
 
-	# merge
-	result_file = open(result_name, 'a')
-	for file in config['files']:
-		file_name = get_file_name(current_path, input_folder, file, config['extension'])
-		try:
-			with open(file_name, 'r') as f:
-				result_file.write(f.read())
-		except Exception as e:
-			print(e)
+	merge_files(result_name, config['files'])
